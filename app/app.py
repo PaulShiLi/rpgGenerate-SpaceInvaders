ebf23d31402ab9d5bd765aefcefd4d1b7b360477
kacperLet <kacperlet27@gmail.com>
2022-07-14 14:29:56 -0400

    Changed model directories

--- app/app.py
+++ app/app.py
@@ -11,6 +11,10 @@ base_url = get_base_url(port)
 def home():
     return render_template('home.html')
 
+@app.route(f'{base_url}/about')
+def about():
+    return render_template('about.html')
+
 @app.route(f'{base_url}/playground', methods=['POST', 'GET'])
 def playground():
     final = ""
@@ -26,7 +30,7 @@ def playground():
         if (model=="personality"):
             if prompt == "":
                 prompt = "They" #sets default personality prompt to "They"
-            ai = aitextgen(model_folder="personality_model/", tokenizer_file="personality_model/aitextgen.tokenizer.json", to_gpu=False)
+            ai = aitextgen(model_folder="model/trained_model/personality_model/", tokenizer_file="model/personality_model/aitextgen.tokenizer.json", to_gpu=False)
             a = ai.generate(4, max_length=100, include_prefix=False, prompt=prompt, return_as_list = True, temperature=randomness)# seperator
             final = []
             for x in a:
@@ -35,8 +39,8 @@ def playground():
             output = final
 
         elif (model == "backstory"):
-            ai = aitextgen(model_folder="backstory_model/", tokenizer_file="backstory_model/aitextgen.tokenizer.json", to_gpu=False)
-            results = ai.generate(4, max_length=100, include_prefix=False, prompt=prompt, return_as_list=True, temperature=randomness)  
+            ai = aitextgen(model_folder="model/backStory_model/", tokenizer_file="model/backStory_model/aitextgen.tokenizer.json", to_gpu=False)
+            results = ai.generate(4, max_length=100, include_prefix=False, prompt=prompt, return_as_list=True, temperature=randomness)
             # seperator
             final = []
             for x in results:
@@ -47,13 +51,15 @@ def playground():
                 texts.append(x[:endIndex])
             print(texts)
             output = texts
-    #     elif (model=="physical"):
-    #        ai = aitextgen(model_folder="physical_model/", tokenizer_file="physical_model/aitextgen.tokenizer.json", to_gpu=False)
-    #        a = ai.generate(4, max_length=100, include_prefix=False, prompt=prompt, return_as_list = True, temperature=randomness)# seperator
-    #        final = []
-    #        for x in a:
-    #            final.append(x.split("\n")[0])
-    #        print(final)
+        elif (model=="physical"):
+            ai = aitextgen(model_folder="model/physical_Model/", tokenizer_file="model/physical_Model/aitextgen.tokenizer.json", to_gpu=False)
+            a = ai.generate(4, max_length=100, include_prefix=False, prompt=prompt, return_as_list = True, temperature=randomness,
+            truncate='<EOS>')# seperator
+            final = []
+            for x in a:
+                final.append(x.split("\n")[0])
+            print(final)
+            output = final
 
         return render_template('playground.html', output=output)
     else:
