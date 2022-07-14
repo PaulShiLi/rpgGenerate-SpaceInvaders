3eec444d7993d80544195dce904f9627a802d467
Paul Li <shi.li.314159@gmail.com>
2022-07-14 11:22:31 -0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -11,6 +11,10 @@ base_url = get_base_url(port)
 def home():
     return render_template('home.html')
 
+@app.route(f'{base_url}/about')
+def about():
+    return render_template('about.html')
+
 @app.route(f'{base_url}/playground', methods=['POST', 'GET'])
 def playground():
     final = ""
@@ -35,8 +39,8 @@ def playground():
             output = final
 
         elif (model == "backstory"):
-            ai = aitextgen(model_folder="backstory_model/", tokenizer_file="backstory_model/aitextgen.tokenizer.json", to_gpu=False)
-            results = ai.generate(4, max_length=100, include_prefix=False, prompt=prompt, return_as_list=True, temperature=randomness)  
+            ai = aitextgen(model_folder="backStory_model/", tokenizer_file="backStory_model/aitextgen.tokenizer.json", to_gpu=False)
+            results = ai.generate(4, max_length=100, include_prefix=False, prompt=prompt, return_as_list=True, temperature=randomness)
             # seperator
             final = []
             for x in results:
@@ -47,13 +51,15 @@ def playground():
                 texts.append(x[:endIndex])
             print(texts)
             output = texts
-    #     elif (model=="physical"):
-    #        ai = aitextgen(model_folder="physical_model/", tokenizer_file="physical_model/aitextgen.tokenizer.json", to_gpu=False)
-    #        a = ai.generate(4, max_length=100, include_prefix=False, prompt=prompt, return_as_list = True, temperature=randomness)# seperator
-    #        final = []
-    #        for x in a:
-    #            final.append(x.split("\n")[0])
-    #        print(final)
+        elif (model=="physical"):
+            ai = aitextgen(model_folder="physical_Model/", tokenizer_file="physical_Model/aitextgen.tokenizer.json", to_gpu=False)
+            a = ai.generate(4, max_length=100, include_prefix=False, prompt=prompt, return_as_list = True, temperature=randomness,
+            truncate='<EOS>')# seperator
+            final = []
+            for x in a:
+                final.append(x.split("\n")[0])
+            print(final)
+            output = final
 
         return render_template('playground.html', output=output)
     else:
